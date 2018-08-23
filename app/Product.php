a026010c396e8185ce1e690aee4cd87afc8a59a4
Ly111999 <39553327+Ly111999@users.noreply.github.com>
2018-08-24 00:00:40 +0700

    update bảng

--- app/Product.php
+++ app/Product.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $table = 'products';
+
+    public function category()
+    {
+        return $this->belongsTo('App\Category', 'categoryId');
+    }
+
+    public function getFormatPriceAttribute()
+    {
+        return sprintf('%s VND', number_format($this->price, 0));
+    }
 }
